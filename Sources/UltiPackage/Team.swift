a451eab17b501987cb1084ff8b5e1eb207f0f6fd
Niuffi <kamirat952@student.polsl.pl>
2022-04-26 00:51:37 +0200

    Team class

---
+++ Sources/UltiPackage/Team.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+class Team: Codable {
+    private(set) var players = [Player]()
+    
+    func addPlayer(name: String, surname: String, gedner: Bool, number: Int) throws {
+        guard !players.contains(where: {$0.number == number}) else {
+            throw TeamError.numberIsInUse
+        }
+    }
+    
+    func removePlayer(number: Int) {
+        players.removeAll(where: {$0.number == number})
+    }
+    
+    enum TeamError: Error {
+        case numberIsInUse
+    }
+}
