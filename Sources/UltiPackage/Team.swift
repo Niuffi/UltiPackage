704662f187638e061d117c737348f3010ead289e
Niuffi <kamirat952@student.polsl.pl>
2022-04-26 18:59:23 +0200

    players @Published

--- Sources/UltiPackage/Team.swift
+++ Sources/UltiPackage/Team.swift
@@ -3,19 +3,45 @@ import Foundation
 @available(iOS 13.0, *)
 @available(macOS 10.15, *)
 public class Team: Codable, ObservableObject {
-    private(set) var players = [Player]()
+    let name: String
+    @Published private(set) var players = [Player]()
     
-    func addPlayer(name: String, surname: String, gedner: Bool, number: Int) throws {
+    public init(name: String) throws {
+        guard name != "" else {
+            throw TeamError.nameIsEmpty
+        }
+        self.name = name
+    }
+    
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        players = try container.decode([Player].self, forKey: .players)
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(players, forKey: .players)
+    }
+    
+    public func addPlayer(name: String, surname: String, gedner: Bool, number: Int) throws {
         guard !players.contains(where: {$0.number == number}) else {
             throw TeamError.numberIsInUse
         }
     }
     
-    func removePlayer(number: Int) {
+    public func removePlayer(number: Int) {
         players.removeAll(where: {$0.number == number})
     }
     
     enum TeamError: Error {
+        case nameIsEmpty
         case numberIsInUse
+    }
+    
+    enum CodingKeys: CodingKey {
+        case name
+        case players
     }
 }
